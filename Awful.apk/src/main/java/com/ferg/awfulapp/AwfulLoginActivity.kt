3e3eaf3fa3287820ffdf07abfba9523c8e3b5de1
Sereri <chr.reiser@gmail.com>
2026-09-02 14:31:54 +0200

    Refactor AwfulLoginActivity

--- Awful.apk/src/main/java/com/ferg/awfulapp/AwfulLoginActivity.kt
+++ Awful.apk/src/main/java/com/ferg/awfulapp/AwfulLoginActivity.kt
@@ -2,7 +2,8 @@
  * *****************************************************************************
  * Copyright (c) 2011, Scott Ferguson
  * All rights reserved.
- * <p/>
+ * 
+ * 
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  * * Redistributions of source code must retain the above copyright
@@ -13,7 +14,8 @@
  * * Neither the name of the software nor the
  * names of its contributors may be used to endorse or promote products
  * derived from this software without specific prior written permission.
- * <p/>
+ * 
+ * 
  * THIS SOFTWARE IS PROVIDED BY SCOTT FERGUSON ''AS IS'' AND ANY
  * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -26,197 +28,181 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  * *****************************************************************************
  */
+package com.ferg.awfulapp
 
-package com.ferg.awfulapp;
+import android.app.ProgressDialog
+import android.graphics.drawable.AnimationDrawable
+import android.os.Bundle
+import android.text.method.LinkMovementMethod
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.TextView.OnEditorActionListener
+import android.widget.Toast
+import com.android.volley.VolleyError
+import com.ferg.awfulapp.Authentication.isUserLoggedIn
+import com.ferg.awfulapp.CaptchaActivity.Companion.handleCaptchaChallenge
+import com.ferg.awfulapp.constants.Constants
+import com.ferg.awfulapp.network.CookieController
+import com.ferg.awfulapp.network.NetworkUtils
+import com.ferg.awfulapp.preferences.AwfulPreferences
+import com.ferg.awfulapp.preferences.Keys
+import com.ferg.awfulapp.task.AwfulRequest.AwfulResultCallback
+import com.ferg.awfulapp.task.LoginRequest
+import org.apache.http.HttpStatus
+import timber.log.Timber.Forest.i
 
-import android.app.Activity;
-import android.app.ProgressDialog;
-import android.graphics.drawable.AnimationDrawable;
-import android.os.Bundle;
-import android.text.method.LinkMovementMethod;
-import android.view.KeyEvent;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.inputmethod.EditorInfo;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.TextView.OnEditorActionListener;
-import android.widget.Toast;
+class AwfulLoginActivity : AwfulActivity() {
+    private var mLogin: Button? = null
+    private var mUsername: EditText? = null
+    private var mPassword: EditText? = null
+    private var mAccept: CheckBox? = null
+    private var mTerms: TextView? = null
 
-import com.android.volley.NetworkResponse;
-import com.android.volley.VolleyError;
-import com.ferg.awfulapp.constants.Constants;
-import com.ferg.awfulapp.network.CookieController;
-import com.ferg.awfulapp.network.NetworkUtils;
-import com.ferg.awfulapp.preferences.AwfulPreferences;
-import com.ferg.awfulapp.preferences.Keys;
-import com.ferg.awfulapp.task.AwfulRequest;
-import com.ferg.awfulapp.task.LoginRequest;
+    private var mDialog: ProgressDialog? = null
 
-import org.apache.http.HttpStatus;
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
-import timber.log.Timber;
+        setContentView(R.layout.login)
 
-public class AwfulLoginActivity extends AwfulActivity {
-    private static final String TAG = "LoginActivity";
+        CookieController.logCookies()
 
-    private Button mLogin;
-    private EditText mUsername;
-    private EditText mPassword;
-    private CheckBox mAccept;
-    private TextView mTerms;
-
-    private ProgressDialog mDialog;
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.login);
-
-        CookieController.logCookies();
-
-        mLogin = (Button) findViewById(R.id.login);
-        mUsername = (EditText) findViewById(R.id.username);
-        mPassword = (EditText) findViewById(R.id.password);
-        mAccept = (CheckBox) findViewById(R.id.login_accept);
-        mTerms = (TextView) findViewById(R.id.login_terms);
-        mPassword.setOnEditorActionListener(new OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if (!mAccept.isChecked()) {
-                    return false;
+        mLogin = findViewById<View?>(R.id.login) as Button
+        mUsername = findViewById<View?>(R.id.username) as EditText
+        mPassword = findViewById<View?>(R.id.password) as EditText
+        mAccept = findViewById<View?>(R.id.login_accept) as CheckBox
+        mTerms = findViewById<View?>(R.id.login_terms) as TextView
+        mPassword?.setOnEditorActionListener(object : OnEditorActionListener {
+            override fun onEditorAction(v: TextView?, actionId: Int, event: KeyEvent?): Boolean {
+                if (mAccept?.isChecked != true) {
+                    return false
                 }
                 if (actionId == EditorInfo.IME_ACTION_DONE) {
-                    loginClick();
+                    loginClick()
                 }
-                if (event != null && event.getKeyCode() == KeyEvent.KEYCODE_ENTER) {
-                    loginClick();
+                if (event?.keyCode == KeyEvent.KEYCODE_ENTER) {
+                    loginClick()
                 }
-                return false;
+                return false
             }
-        });
+        })
 
-        mLogin.setOnClickListener(onLoginClick);
+        mLogin?.setOnClickListener(onLoginClick)
 
-        mUsername.requestFocus();
+        mUsername?.requestFocus()
 
-        final ImageView image = (ImageView) findViewById(R.id.dealwithit);
-        image.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ((AnimationDrawable) image.getDrawable()).start();
+        val image = findViewById<View?>(R.id.dealwithit) as ImageView
+        image.setOnClickListener { (image.drawable as AnimationDrawable).start() }
+
+        mTerms?.movementMethod = LinkMovementMethod.getInstance()
+
+        mAccept?.setOnClickListener {
+            mLogin?.let { login ->
+                mAccept?.let { accept -> login.isEnabled = accept.isChecked }
             }
-        });
-
-        mTerms.setMovementMethod(LinkMovementMethod.getInstance());
-
-        mAccept.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mLogin.setEnabled(mAccept.isChecked());
-            }
-        });
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        Timber.i("onResume");
-        if (Authentication.INSTANCE.isUserLoggedIn()) {
-            Timber.i("Already logged in! Closing AwfulLoginActivity!");
-            this.finish();
         }
     }
 
-    @Override
-    public void onPause() {
-        super.onPause();
-
-        if (mDialog != null) {
-            mDialog.dismiss();
+    public override fun onResume() {
+        super.onResume()
+        i("onResume")
+        if (isUserLoggedIn()) {
+            i("Already logged in! Closing AwfulLoginActivity!")
+            this.finish()
         }
     }
 
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        if (mDialog != null) {
-            mDialog.dismiss();
-        }
+    public override fun onPause() {
+        super.onPause()
+        mDialog?.dismiss()
     }
 
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
+    public override fun onStop() {
+        super.onStop()
+        mDialog?.dismiss()
 
-        if (mDialog != null) {
-            mDialog.dismiss();
-        }
     }
 
-    private void loginClick() {
-        final String username = NetworkUtils.encodeHtml(mUsername.getText().toString());
-        final String password = NetworkUtils.encodeHtml(mPassword.getText().toString());
+    public override fun onDestroy() {
+        super.onDestroy()
+        mDialog?.dismiss()
+    }
 
-        mDialog = ProgressDialog.show(AwfulLoginActivity.this, "Logging In", "Hold on...", true);
-        setPreferredFont(mDialog.findViewById(android.R.id.title));
-        final AwfulLoginActivity self = this;
-        NetworkUtils.queueRequest(new LoginRequest(this, username, password).build(null, new AwfulRequest.AwfulResultCallback<Boolean>() {
-            @Override
-            public void success(Boolean result) {
-                onLoginSuccess();
-            }
+    private fun loginClick() {
+        val username = NetworkUtils.encodeHtml(mUsername?.text.toString())
+        val password = NetworkUtils.encodeHtml(mPassword?.text.toString())
 
-            @Override
-            public void failure(VolleyError error) {
-                CaptchaActivity.handleCaptchaChallenge(self, error);
-
-                // Volley sometimes generates NetworkErrors with no response set, or wraps them
-                NetworkResponse response = error.networkResponse;
-                if (response == null) {
-                    Throwable cause = error.getCause();
-                    if (cause != null && cause instanceof VolleyError) {
-                        response = ((VolleyError) cause).networkResponse;
+        mDialog = ProgressDialog.show(this@AwfulLoginActivity, "Logging In", "Hold on...", true)
+        setPreferredFont(mDialog?.findViewById<View?>(android.R.id.title))
+        val self = this
+        NetworkUtils.queueRequest(
+            LoginRequest(this, username, password).build(
+                null,
+                object : AwfulResultCallback<Boolean> {
+                    override fun success(result: Boolean) {
+                        onLoginSuccess()
                     }
-                }
-                if (response != null && response.statusCode == HttpStatus.SC_MOVED_TEMPORARILY) {
-                    Boolean result = !CookieController.getCookieString(Constants.COOKIE_PREF_PASSWORD).isEmpty();
-                    if (result) {
-                        // TODO: this should probably be handled by firing a ProfileRequest and getting the username from there, maybe through SyncManager
-                        AwfulPreferences prefs = AwfulPreferences.getInstance(getApplicationContext());
-                        prefs.setPreference(Keys.USERNAME, username);
-                        onLoginSuccess();
-                    } else {
-                        onLoginFailed();
+                    override fun failure(error: VolleyError?) {
+                        handleCaptchaChallenge(self, error!!)
+
+                        // Volley sometimes generates NetworkErrors with no response set, or wraps them
+                        var response = error.networkResponse
+                        if (response == null) {
+                            val cause = error.cause
+                            if (cause != null && cause is VolleyError) {
+                                response = cause.networkResponse
+                            }
+                        }
+                        if (response != null && response.statusCode == HttpStatus.SC_MOVED_TEMPORARILY) {
+                            val result =
+                                !CookieController.getCookieString(Constants.COOKIE_PREF_PASSWORD)
+                                    .isEmpty()
+                            if (result) {
+                                // TODO: this should probably be handled by firing a ProfileRequest and getting the username from there, maybe through SyncManager
+                                val prefs = AwfulPreferences.getInstance(applicationContext)
+                                prefs.setPreference(Keys.USERNAME, username)
+                                onLoginSuccess()
+                            } else {
+                                onLoginFailed()
+                            }
+                        } else {
+                            onLoginFailed()
+                        }
                     }
-                } else {
-                    onLoginFailed();
-                }
-            }
 
-            private void onLoginSuccess() {
-                mDialog.dismiss();
-                Toast.makeText(AwfulLoginActivity.this, R.string.login_succeeded, Toast.LENGTH_SHORT).show();
-                setResult(Activity.RESULT_OK);
-                self.finish();
-            }
+                    fun onLoginSuccess() {
+                        mDialog?.dismiss()
+                        Toast.makeText(
+                            this@AwfulLoginActivity,
+                            R.string.login_succeeded,
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        setResult(RESULT_OK)
+                        self.finish()
+                    }
 
-            private void onLoginFailed() {
-                mDialog.dismiss();
-                Toast.makeText(AwfulLoginActivity.this, R.string.login_failed, Toast.LENGTH_SHORT).show();
-                setResult(Activity.RESULT_CANCELED);
-            }
-        }));
+                    fun onLoginFailed() {
+                        mDialog?.dismiss()
+                        Toast.makeText(
+                            this@AwfulLoginActivity,
+                            R.string.login_failed,
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        setResult(RESULT_CANCELED)
+                    }
+                })
+        )
     }
 
-    private View.OnClickListener onLoginClick = new View.OnClickListener() {
-        public void onClick(View aView) {
-            loginClick();
-        }
-    };
+    private val onLoginClick: View.OnClickListener = View.OnClickListener { loginClick() }
+
+    companion object {
+        private const val TAG = "LoginActivity"
+    }
 }

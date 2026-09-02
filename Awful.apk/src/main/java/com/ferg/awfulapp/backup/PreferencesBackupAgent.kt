c5745100f5f9a6ee5be7f11afa322b99c368ee40
Sereri <chr.reiser@gmail.com>
2026-09-02 18:27:04 +0200

    Refactor PreferencesBackupAgent

--- Awful.apk/src/main/java/com/ferg/awfulapp/backup/PreferencesBackupAgent.kt
+++ Awful.apk/src/main/java/com/ferg/awfulapp/backup/PreferencesBackupAgent.kt
@@ -1,20 +1,19 @@
-package com.ferg.awfulapp.backup;
+package com.ferg.awfulapp.backup
 
-import android.app.backup.BackupAgentHelper;
-import android.app.backup.SharedPreferencesBackupHelper;
+import android.app.backup.BackupAgentHelper
+import android.app.backup.SharedPreferencesBackupHelper
+import com.ferg.awfulapp.constants.Constants
 
-import com.ferg.awfulapp.constants.Constants;
+class PreferencesBackupAgent : BackupAgentHelper() {
+    companion object {
+        //if changing package name, MAKE SURE TO GET THIS TOO.
+        //com.example.appname_preferences
+        private const val DEFAULT_PREFERENCES = "com.ferg.awfulapp_preferences"
+        private const val BACKUP_KEY = "preferences_backup"
+    }
 
-public class PreferencesBackupAgent extends BackupAgentHelper {
-	//if changing package name, MAKE SURE TO GET THIS TOO.
-	//com.example.appname_preferences
-    private static final String DEFAULT_PREFERENCES = "com.ferg.awfulapp_preferences";
-    private static final String BACKUP_KEY          = "preferences_backup";
-
-    @Override
-    public void onCreate() {
-        SharedPreferencesBackupHelper helper = 
-            new SharedPreferencesBackupHelper(this, DEFAULT_PREFERENCES, Constants.COOKIE_PREFERENCE);
-        addHelper(BACKUP_KEY, helper);
+    override fun onCreate() {
+        val helper = SharedPreferencesBackupHelper(this, DEFAULT_PREFERENCES, Constants.COOKIE_PREFERENCE)
+        addHelper(BACKUP_KEY, helper)
     }
 }

5b2b4fc8473af13f0e33c3f418297148c0218764
Sereri <chr.reiser@gmail.com>
2026-09-07 11:24:28 +0200

    Refactor SyncManager

--- Awful.apk/src/main/java/com/ferg/awfulapp/sync/SyncManager.kt
+++ Awful.apk/src/main/java/com/ferg/awfulapp/sync/SyncManager.kt
@@ -1,179 +1,184 @@
-package com.ferg.awfulapp.sync;
+package com.ferg.awfulapp.sync
 
-import android.content.Context;
-import android.os.Handler;
-import android.os.Looper;
-import androidx.annotation.NonNull;
-import android.widget.Toast;
-
-import com.ferg.awfulapp.Authentication;
-import com.ferg.awfulapp.R;
-import com.ferg.awfulapp.announcements.AnnouncementsManager;
-import com.ferg.awfulapp.forums.CrawlerTask;
-import com.ferg.awfulapp.forums.DropdownParserTask;
-import com.ferg.awfulapp.forums.ForumRepository;
-import com.ferg.awfulapp.messages.PmManager;
-import com.ferg.awfulapp.network.NetworkUtils;
-import com.ferg.awfulapp.preferences.AwfulPreferences;
-import com.ferg.awfulapp.preferences.LongPreference;
-import com.ferg.awfulapp.preferences.StringPreference;
-import com.ferg.awfulapp.task.FeatureRequest;
-import com.ferg.awfulapp.task.RefreshUserProfileRequest;
-import com.ferg.awfulapp.util.AwfulUtils;
-
-import java.util.concurrent.TimeUnit;
-
-import timber.log.Timber;
+import android.content.Context
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
+import com.ferg.awfulapp.Authentication.isUserLoggedIn
+import com.ferg.awfulapp.R
+import com.ferg.awfulapp.announcements.AnnouncementsManager
+import com.ferg.awfulapp.forums.CrawlerTask
+import com.ferg.awfulapp.forums.DropdownParserTask
+import com.ferg.awfulapp.forums.ForumRepository
+import com.ferg.awfulapp.forums.ForumRepository.ForumsUpdateListener
+import com.ferg.awfulapp.messages.PmManager
+import com.ferg.awfulapp.network.NetworkUtils.queueRequest
+import com.ferg.awfulapp.preferences.AwfulPreferences
+import com.ferg.awfulapp.preferences.LongPreference
+import com.ferg.awfulapp.preferences.StringPreference
+import com.ferg.awfulapp.task.FeatureRequest
+import com.ferg.awfulapp.task.RefreshUserProfileRequest
+import com.ferg.awfulapp.util.AwfulUtils
+import timber.log.Timber.Forest.d
+import timber.log.Timber.Forest.i
+import timber.log.Timber.Forest.w
+import java.util.Locale
+import java.util.concurrent.TimeUnit
+import kotlin.concurrent.Volatile
 
 /**
  * Created by baka kaba on 29/04/2016.
- * <p/>
+ * 
+ * 
  * Class for handling general data sync tasks and housekeeping.
  */
-public class SyncManager {
+object SyncManager {
+    private const val FORUM_UPDATE_FREQUENCY = 1
+    private val FORUM_UPDATE_FREQUENCY_UNITS = TimeUnit.DAYS
 
-    private static final int FORUM_UPDATE_FREQUENCY = 1;
-    private static final TimeUnit FORUM_UPDATE_FREQUENCY_UNITS = TimeUnit.DAYS;
 
-
-    public static void sync(@NonNull Context context) {
-        Context appContext = context.getApplicationContext();
-        if (!Authentication.INSTANCE.isUserLoggedIn()) {
-            Timber.w("Failed to sync - user is not logged in");
-            return;
+    fun sync(context: Context) {
+        val appContext = context.applicationContext
+        if (!isUserLoggedIn()) {
+            w("Failed to sync - user is not logged in")
+            return
         }
-        Timber.i("------ syncing profile and forum details");
-        updateProfile(appContext);
-        updateAccountFeatures(appContext);
-        updatePms(appContext);
-        updateAnnouncements(appContext);
-        updateForums(appContext);
-        trimDatabase(appContext);
-        updateImgur(appContext);
+        i("------ syncing profile and forum details")
+        updateProfile(appContext)
+        updateAccountFeatures(appContext)
+        updatePms(appContext)
+        updateAnnouncements(appContext)
+        updateForums(appContext)
+        trimDatabase(appContext)
+        updateImgur(appContext)
     }
 
 
-    private static void updateAccountFeatures(@NonNull Context context) {
-        NetworkUtils.queueRequest(new FeatureRequest(context).build(null, null));
+    private fun updateAccountFeatures(context: Context) {
+        queueRequest(FeatureRequest(context).build(null, null))
     }
 
 
-    private static void updatePms(@NonNull Context context) {
-        PmManager.updatePms(context);
+    private fun updatePms(context: Context) {
+        PmManager.updatePms(context)
     }
 
 
-    private static void updateAnnouncements(@NonNull Context context) {
+    private fun updateAnnouncements(context: Context) {
         // rubbish hack to avoid the Announcements and PM snackbars from appearing simultaneously and cancelling each other
-        new Handler(Looper.getMainLooper())
-                .postDelayed(() -> AnnouncementsManager.updateAnnouncements(context), 10_000L);
+        Handler(Looper.getMainLooper())
+            .postDelayed(Runnable { AnnouncementsManager.updateAnnouncements(context) }, 10000L)
     }
 
 
-    private static void updateProfile(@NonNull Context context) {
-        NetworkUtils.queueRequest(new RefreshUserProfileRequest(context).build(null, null));
+    private fun updateProfile(context: Context) {
+        queueRequest(RefreshUserProfileRequest(context).build(null, null))
     }
 
 
-    private static void trimDatabase(@NonNull Context context) {
-        AwfulUtils.trimDbEntries(context.getContentResolver());
+    private fun trimDatabase(context: Context) {
+        AwfulUtils.trimDbEntries(context.contentResolver)
     }
 
 
-    private static void updateForums(@NonNull final Context context) {
-        final ForumRepository forumRepo = ForumRepository.getInstance(context);
+    private fun updateForums(context: Context) {
+        val forumRepo = ForumRepository.getInstance(context)
         // cancel any update that's in progress
-        forumRepo.cancelUpdate();
+        forumRepo.cancelUpdate()
 
-        boolean hasForumData = forumRepo.hasForumData();
-        long lastSuccessfulUpdate = forumRepo.getLastRefreshTime();
-        long timeSinceUpdate = FORUM_UPDATE_FREQUENCY_UNITS.convert(System.currentTimeMillis() - lastSuccessfulUpdate, TimeUnit.MILLISECONDS);
-        String timeUnits = FORUM_UPDATE_FREQUENCY_UNITS.toString().toLowerCase();
+        val hasForumData = forumRepo.hasForumData()
+        val lastSuccessfulUpdate = forumRepo.lastRefreshTime
+        val timeSinceUpdate = FORUM_UPDATE_FREQUENCY_UNITS.convert(
+            System.currentTimeMillis() - lastSuccessfulUpdate,
+            TimeUnit.MILLISECONDS
+        )
+        val timeUnits = FORUM_UPDATE_FREQUENCY_UNITS.toString().lowercase(Locale.getDefault())
         // TODO: add better data limiting, maybe as a separate settings category / general 'restrict data' option
-        boolean limitDataUse = !AwfulPreferences.getInstance(context).canLoadImages();
+        val limitDataUse = !AwfulPreferences.getInstance(context).canLoadImages()
 
         // work out if we're due an update
-        boolean updateDue = timeSinceUpdate >= FORUM_UPDATE_FREQUENCY;
+        val updateDue = timeSinceUpdate >= FORUM_UPDATE_FREQUENCY
 
         // unless we really need some forum data (e.g. after a data clear), only update if scheduled and permitted
         if (hasForumData && (limitDataUse || !updateDue)) {
-                Timber.d("Not updating forums - %s %s since last update", timeSinceUpdate, timeUnits);
-            return;
+            d("Not updating forums - %s %s since last update", timeSinceUpdate, timeUnits)
+            return
         }
-        CrawlerTask.Priority updatePriority = hasForumData ? CrawlerTask.Priority.LOW : CrawlerTask.Priority.HIGH;
-            Timber.d("Updating forums (%s priority) - %s forum data, %d %s since last update",
-                    updatePriority.name(),
-                    hasForumData ? "we have old" : "no existing",
-                    timeSinceUpdate,
-                    timeUnits);
+        val updatePriority =
+            if (hasForumData) CrawlerTask.Priority.LOW else CrawlerTask.Priority.HIGH
+        d(
+            "Updating forums (%s priority) - %s forum data, %d %s since last update",
+            updatePriority.name,
+            if (hasForumData) "we have old" else "no existing",
+            timeSinceUpdate,
+            timeUnits
+        )
 
         // add a listener for the result - this is really to check for failure in a no-data situation
-        forumRepo.registerListener(new UpdateResultHandler(forumRepo, context));
+        forumRepo.registerListener(UpdateResultHandler(forumRepo, context))
 
         // finally, now the handler is set up, start the update task
-        forumRepo.updateForums(new CrawlerTask(context, updatePriority));
+        forumRepo.updateForums(CrawlerTask(context, updatePriority))
     }
 
-    private static void updateImgur(@NonNull final Context context){
-        AwfulPreferences mPrefs = AwfulPreferences.getInstance(context);
-        if ( System.currentTimeMillis() > mPrefs.imgurTokenExpires) {
-            mPrefs.setPreference(StringPreference.IMGUR_ACCOUNT_TOKEN, (String) null);
-            mPrefs.setPreference(StringPreference.IMGUR_REFRESH_TOKEN, (String) null);
-            mPrefs.setPreference(StringPreference.IMGUR_ACCOUNT, (String) null);
-            mPrefs.setPreference(LongPreference.IMGUR_TOKEN_EXPIRES, 0L);
+    private fun updateImgur(context: Context) {
+        val mPrefs = AwfulPreferences.getInstance(context)
+        if (System.currentTimeMillis() > mPrefs.imgurTokenExpires) {
+            mPrefs.setPreference(StringPreference.IMGUR_ACCOUNT_TOKEN, null as String?)
+            mPrefs.setPreference(StringPreference.IMGUR_REFRESH_TOKEN, null as String?)
+            mPrefs.setPreference(StringPreference.IMGUR_ACCOUNT, null as String?)
+            mPrefs.setPreference(LongPreference.IMGUR_TOKEN_EXPIRES, 0L)
         }
     }
 
 
-    private static class UpdateResultHandler implements ForumRepository.ForumsUpdateListener {
-
-        private final ForumRepository forumRepo;
-        private final Context context;
+    private class UpdateResultHandler(
+        private val forumRepo: ForumRepository,
+        private val context: Context
+    ) : ForumsUpdateListener {
         // flag to check if the fallback DropdownParse task has been run yet
-        volatile boolean parsedDropdown;
+        @Volatile
+        var parsedDropdown: Boolean = false
 
 
-        UpdateResultHandler(ForumRepository forumRepo, Context context) {
-            this.forumRepo = forumRepo;
-            this.context = context;
-            parsedDropdown = false;
+        override fun onForumsUpdateStarted() {
         }
 
 
-        @Override
-        public void onForumsUpdateStarted() {
-        }
-
-
-        @Override
-        public void onForumsUpdateCompleted(boolean success) {
+        override fun onForumsUpdateCompleted(success: Boolean) {
             // check for a serious failure where we have no data, and run the basic dropdown update once if necessary
-            boolean noForumData = !forumRepo.hasForumData();
-            String message = "onForumsUpdateCompleted: sync %s, no forum data: %b, dropdown parse run: %b";
-            Timber.i(message, success ? "succeeded" : "failed", noForumData, parsedDropdown);
+            val noForumData = !forumRepo.hasForumData()
+            val message =
+                "onForumsUpdateCompleted: sync %s, no forum data: %b, dropdown parse run: %b"
+            i(message, if (success) "succeeded" else "failed", noForumData, parsedDropdown)
 
             if (noForumData && !parsedDropdown) {
-                Timber.w("Forum update failed, still have no data - running dropdown parser to get something");
-                parsedDropdown = true;
+                w("Forum update failed, still have no data - running dropdown parser to get something")
+                parsedDropdown = true
                 // TODO: other callbacks are out of order, since other *Completed callbacks follow this, but this triggers some *Started ones first
                 // basically the problem is, the index fragment gets a callback for this NEW update before it gets
                 // the completed callback for the OLD one. So it cancels the progress bar and doesn't restart it
-                forumRepo.updateForums(new DropdownParserTask(context));
+                forumRepo.updateForums(DropdownParserTask(context))
             } else {
                 if (noForumData) {
-                    Handler handler = new Handler(context.getMainLooper());
-                    handler.post(() -> Toast.makeText(context, R.string.forums_update_failure_message, Toast.LENGTH_LONG).show());
+                    val handler = Handler(context.mainLooper)
+                    handler.post(Runnable {
+                        Toast.makeText(
+                            context,
+                            R.string.forums_update_failure_message,
+                            Toast.LENGTH_LONG
+                        ).show()
+                    })
                 }
-                forumRepo.unregisterListener(this);
+                forumRepo.unregisterListener(this)
             }
         }
 
 
-        @Override
-        public void onForumsUpdateCancelled() {
+        override fun onForumsUpdateCancelled() {
             // we need to ditch this listener if the update was cancelled,
             // since a new update will add another with fresh state
-            forumRepo.unregisterListener(this);
+            forumRepo.unregisterListener(this)
         }
     }
 }
